4027780fc15f9cb2b9c68ce60d492fbf7e45947d
Lassalle Cedric <lassallc@grenoble-inp.org>
2024-09-30 15:17:52 +0200

    feat: import spring project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# backend-ideas-manager
-Back of the ideas-manager app, users can register and organized their ideas
+# Backend Ideas Manager
+
+Back of the ideas-manager app, users can register and organized their ideas.
+
+
---
+++ src/main/java/com/shapirogrill/ideasmanager/IdeasmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.shapirogrill.ideasmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeasmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeasmanagerApplication.class, args);
+	}
+
+}
